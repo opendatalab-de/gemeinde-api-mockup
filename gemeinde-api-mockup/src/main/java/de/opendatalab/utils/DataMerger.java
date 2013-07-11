29df9316a0e95b1ae8a2299bc53c00238f543108
Adrian Stabiszewski <as@grundid.de>
2013-07-11 16:53:16 +0200

    rename

--- gemeinde-api-mockup/src/main/java/de/opendatalab/utils/DataMerger.java
+++ gemeinde-api-mockup/src/main/java/de/opendatalab/utils/DataMerger.java
@@ -7,7 +7,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
-public class Realsteuervergleich {
+public class DataMerger {
 
 	private static final Map<String, ParserConfiguration> CONFIGS = new HashMap<String, ParserConfiguration>();
 	static {
